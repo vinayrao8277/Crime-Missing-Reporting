65a5505dce26e83585f5237c0a7f92c6d8592089
vinayrao8277 <vinayrao8277@.com>
2022-06-09 14:29:16 +0530

    initial commit

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Crime And Missing People</title>
+  <link rel="stylesheet" type="text/css" href="home.css">
+  
+</head>
+<body>
+  <h1 class="head"> Crime And Missing Portal</h1>
+  <div>
+    <ul>
+      <li><a class="home" href="home.html">Home</a></li>
+      <li class="user" href="userlogin.php">User Login</li>
+      <li class="official" href="official.php">Official Login</li>
+    </ul>
+  </div>
+  <div class="button">
+  <button class="submit" href="signup.php">Sign Up</button>
+</div>
+</body>
+
+</html>
